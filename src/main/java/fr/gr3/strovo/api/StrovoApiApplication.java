fba1445de40585dd1c4ec73c831c2bd1880d5ef2
ROMAIN TRIMOUILLE <romain.trimouille@iut.local>
2024-01-22 14:38:19 +0100

    Initialisation

---
+++ src/main/java/fr/gr3/strovo/api/StrovoApiApplication.java
@@ -0,0 +1,13 @@
+package fr.gr3.strovo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrovoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrovoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/gr3/strovo/api/StrovoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gr3.strovo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrovoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
