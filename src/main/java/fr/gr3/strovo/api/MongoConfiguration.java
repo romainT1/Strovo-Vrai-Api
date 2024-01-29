e8ceac58976dd7e63b9a5bb4381a9d407361306b
Aymeric Thevenet <aymeric.thevenet@iut-rodez.fr>
2024-01-29 14:41:24 +0100

    Ajout de la configuration pour MongoDb

---
+++ src/main/java/fr/gr3/strovo/api/MongoConfiguration.java
@@ -0,0 +1,40 @@
+package fr.gr3.strovo.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+
+/**
+ * Classe de configuration pour MongoDB dans une application Spring.
+ */
+@Configuration
+public class MongoConfiguration {
+
+    /** Injection de dépendance MongoDatabaseFactory. */
+    @Autowired
+    MongoDatabaseFactory mongoDbFactory;
+
+    /** Injection de dépendance MongoMappingContext. */
+    @Autowired
+    MongoMappingContext mongoMappingContext;
+
+    /**
+     * Configure et fournit un bean MappingMongoConverter.
+     * Le MappingMongoConverter est responsable de la conversion des documents MongoDB en objets Java
+     * et vice versa. Il utilise le MongoDatabaseFactory et le MongoMappingContext fournis.
+     * @return Bean MappingMongoConverter configuré
+     */
+    @Bean
+    public MappingMongoConverter mappingMongoConverter() {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
+}
