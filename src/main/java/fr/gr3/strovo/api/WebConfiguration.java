8c29e34d592ac2ede48a2961de1bcf0f0494e01b
Romain TRIMOUILLE <romain.trimouille@iut-rodez.fr>
2024-02-05 14:25:39 +0100

    Ajout de l'intercepteur de requête

---
+++ src/main/java/fr/gr3/strovo/api/WebConfiguration.java
@@ -0,0 +1,20 @@
+package fr.gr3.strovo.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration web de l'api.
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    /**
+     * Ajoute les intercepteurs de requêtes.
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
+    }
+}
